4124fa0541ba0725fea6a012983c3ace00e6851c
lakshan <lakshanrashmika16302@gmail.com>
2024-05-09 09:54:25 +0530

    updated

---
+++ Creational/src/main/java/lk/ijse/desingPatten/Main.java
@@ -0,0 +1,14 @@
+package lk.ijse.desingPatten;
+
+/**
+ * @version: v0.0.1
+ * @author: lakshanR
+ * @date: 5/9/2024
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton obj = Singleton.getInstance();
+
+    }
+}
---
+++ Creational/src/main/java/lk/ijse/desingPatten/Singleton.java
@@ -0,0 +1,20 @@
+package lk.ijse.desingPatten;
+
+/**
+ * @version: v0.0.1
+ * @author: LakshanR
+ * @date: 5/9/2024
+ */
+
+public class Singleton {
+
+    private static Singleton instance = new Singleton();
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        return instance;
+    }
+
+
+}
